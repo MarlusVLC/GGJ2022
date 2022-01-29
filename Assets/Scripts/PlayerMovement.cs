3416c9c002f03ee4949e208721c2588076521e37
Choosecake <imchoosecake@gmail.com>
2022-01-29 14:04:08 -0300

    makes camera movement work correctly (missing some adjustments)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private Transform cam;
     private Rigidbody rb;
     private Vector3 movement;
     private Quaternion rotation = Quaternion.identity;
     private LayerMask groundLayer;
-    private float turnSpeed = 21f;
+    private float turnSpeed = 20f;
+    private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
     private float moveSpeed = 5f;
     private float jumpForce = 5f;
     private bool isGrounded;
@@ -26,15 +29,19 @@ public class PlayerMovement : MonoBehaviour
         movement.Set(horizontal, 0f, vertical);
         movement.Normalize();
 
+        float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
         bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
         bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
         bool isWalking = hasHorizontalInput || hasVerticalInput; 
        
-        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
-        rotation = Quaternion.LookRotation(desiredForward);
-
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
-        rb.MoveRotation(rotation);
+        if (isWalking)
+        {
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            rb.MovePosition(rb.position + moveDir.normalized * moveSpeed * Time.deltaTime);
+        }
 
         // jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
